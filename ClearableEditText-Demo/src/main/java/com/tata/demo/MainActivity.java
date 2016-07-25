0ffed90b9d105206760bff2c226e32c4ce9c256c
liuzhanta <liuzhanta@huoban.com>
2016-07-25 21:57:07 +0800

    +commit

--- ClearableEditText-Demo/src/main/java/com/tata/demo/MainActivity.java
+++ ClearableEditText-Demo/src/main/java/com/tata/demo/MainActivity.java
@@ -3,7 +3,7 @@ package com.tata.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.zterry.clearableedittext.R;
+import com.tata.demo.clearableedittext.R;
 
 
 public class MainActivity extends AppCompatActivity {
